eece039add8eb2092eb5c5ffa65f19466e426675
anouar garbaa <g.anouar@yahoo.com>
2025-06-30 17:50:55 +0200

    Handling req & res

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -1,14 +1,35 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  Post,
+} from '@nestjs/common';
 
 @Controller('coffees')
 export class CoffeesController {
-  @Get('flavors')
+  @Get()
   findAll() {
-    return 'here we return all coffees';
+    return 'This action returns all coffees';
   }
+  // findAll(@Res() response) {
+  //   response.status(200).send('This action returns all coffees');
+  // }
 
   @Get(':id')
   findOne(@Param('id') id: string) {
     return `here we return ${id} coffee`;
   }
+
+  @Post()
+  create(@Body() body) {
+    return body;
+  }
+  //  @Post()
+  // @HttpCode(HttpStatus.GONE)
+  // create(@Body() body) {
+  //   return body;
+  // }
 }
