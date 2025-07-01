8d0564a99898bd3800dc8ce71ff6ade41b69fe8c
anouar garbaa <g.anouar@yahoo.com>
2025-07-01 02:31:23 +0200

    Configure TypeORM

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,31 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CoffeesModule } from './coffees/coffees.module';
 
 @Module({
-  imports: [CoffeesModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true, // make variables available within all the app
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get('POSTGRES_HOST'),
+        port: parseInt(configService.get('POSTGRES_PORT') ?? '5432', 10),
+        username: configService.get('POSTGRES_USER'),
+        password: configService.get('POSTGRES_PASSWORD'),
+        database: configService.get('POSTGRES_DB'),
+        autoLoadEntities: true,
+        synchronize: true,
+      }),
+    }),
+    CoffeesModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
