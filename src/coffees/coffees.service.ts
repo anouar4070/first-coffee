f0e8ad9a1dafe1e6e746d2f810d935e052691370
anouar garbaa <g.anouar@yahoo.com>
2025-07-02 16:51:43 +0200

    Retrieve entities with their Relations

--- src/coffees/coffees.service.ts
+++ src/coffees/coffees.service.ts
@@ -13,13 +13,18 @@ export class CoffeesService {
   ) {}
 
   findAll() {
-    return this.coffeeRepository.find();
+    return this.coffeeRepository.find({
+      relations: ['flavors'],
+    });
   }
 
   async findOne(id: string) {
     //const coffee = this.coffees.find((item) => item.id === +id);
     //const coffee = await this.coffeeRepository.findOneBy({ id: +id });
-    const coffee = await this.coffeeRepository.findOne({ where: { id: +id } });
+    const coffee = await this.coffeeRepository.findOne({
+      where: { id: +id },
+      relations: ['flavors'],
+    });
     if (!coffee) {
       //throw new HttpException(`Coffee #${id} not found`, HttpStatus.NOT_FOUND);
       throw new NotFoundException(`Coffee #${id} not found`);
