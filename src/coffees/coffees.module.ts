81c21f0e3c0fa359f262dfb9b20b94354034b048
anouar garbaa <g.anouar@yahoo.com>
2025-07-03 19:27:10 +0200

    add a Nonclass based Provider Tokens

--- src/coffees/coffees.module.ts
+++ src/coffees/coffees.module.ts
@@ -5,13 +5,19 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Coffee } from './entities/coffee.entity';
 import { Flavor } from './entities/flavor.entity/flavor.entity';
 import { Event } from 'src/events/entities/event.entity/event.entity';
+import { COFFEE_BRANDS } from './coffees.constants';
 
-class MockCoffeesService {}
+//class MockCoffeesService {}
 
 @Module({
   imports: [TypeOrmModule.forFeature([Coffee, Flavor, Event])],
   controllers: [CoffeesController],
-  providers: [{ provide: CoffeesService, useValue: new MockCoffeesService() }],
+  //providers: [CoffeesService],
+  //providers: [{ provide: CoffeesService, useValue: new MockCoffeesService() }],
+  providers: [
+    CoffeesService,
+    { provide: COFFEE_BRANDS, useValue: ['buddy brew', 'nescafe'] },
+  ],
   exports: [CoffeesService],
 })
 export class CoffeesModule {}
