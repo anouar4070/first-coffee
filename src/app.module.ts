a5fa2bd69bda831fc70c09570ffb8ca4dcb927d0
anouar garbaa <g.anouar@yahoo.com>
2025-07-03 16:44:43 +0200

    control Nest Module Encapsulation

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CoffeesModule } from './coffees/coffees.module';
+import { CoffeeRatingModule } from './coffee-rating/coffee-rating.module';
 
 @Module({
   imports: [
@@ -25,6 +26,7 @@ import { CoffeesModule } from './coffees/coffees.module';
       }),
     }),
     CoffeesModule,
+    CoffeeRatingModule,
   ],
   controllers: [AppController],
   providers: [AppService],
