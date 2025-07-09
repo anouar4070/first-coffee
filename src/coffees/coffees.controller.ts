9ee47574a5c0f87624fab59bffa646a5cdf16bd8
anouar garbaa <g.anouar@yahoo.com>
2025-07-09 18:05:21 +0200

    using Binding techniques

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -3,27 +3,31 @@ import {
   Controller,
   Delete,
   Get,
-  Inject,
+  //Inject,
   Param,
   Patch,
   Post,
   Query,
+  UsePipes,
+  ValidationPipe,
 } from '@nestjs/common';
 import { CoffeesService } from './coffees.service';
 import { CreateCoffeeDto } from './dto/create-coffee.dto/create-coffee.dto';
 import { UpdateCoffeeDto } from './dto/update-coffee.dto/update-coffee.dto';
 import { PaginationQueryDto } from 'src/common/dto/pagination-query.dto/pagination-query.dto';
-import { REQUEST } from '@nestjs/core';
+//import { REQUEST } from '@nestjs/core';
 
+//@UsePipes(ValidationPipe)
 @Controller('coffees')
 export class CoffeesController {
   constructor(
     private readonly coffeesService: CoffeesService,
-    @Inject(REQUEST) private readonly request: Request,
+    //@Inject(REQUEST) private readonly request: Request,
   ) {
     console.log('CoffeesController created');
   }
 
+  //@UsePipes(ValidationPipe)
   @Get()
   findAll(@Query() paginationQuery: PaginationQueryDto) {
     //const { limit, offset } = paginationQuery;
@@ -54,7 +58,10 @@ export class CoffeesController {
   // }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateCoffeeDto: UpdateCoffeeDto) {
+  update(
+    @Param('id') id: string,
+    @Body(ValidationPipe) updateCoffeeDto: UpdateCoffeeDto,
+  ) {
     return this.coffeesService.update(id, updateCoffeeDto);
     //return `This action updates ${id} coffee`;
   }
